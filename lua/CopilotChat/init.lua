b34a78f05ebe65ca093e4dc4b66de9120a681f4c
Huynh Duc Dung <dunghd.it@gmail.com>
2023-12-23 14:27:52 +0800

    feat: add CCTests command
    chore: briefly explain the code before adding unit tests

--- lua/CopilotChat/init.lua
+++ lua/CopilotChat/init.lua
@@ -8,6 +8,16 @@ M.setup = function()
   utils.create_cmd('CChatExplain', function(opts)
     vim.cmd('CChat Explain how it works')
   end, { nargs = '*', range = true })
+
+  -- Add new command to generate unit tests with CopilotChat for selected text
+  utils.create_cmd('CChatTests', function(opts)
+    local cmd = 'CChat Briefly explain how it works then generate unit tests for the code.'
+    -- Append the provided arguments to the command if any
+    if opts.args then
+      cmd = cmd .. ' ' .. opts.args
+    end
+    vim.cmd(cmd)
+  end, { nargs = '*', range = true })
 end
 
 return M
