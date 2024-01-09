db879715076ed227a3be330d0bad857ce5cccb55
gptlang <acheong@alumni.dalat.org>
2024-01-09 01:36:04 +0000

    close #12

--- rplugin/python3/copilot.py
+++ rplugin/python3/copilot.py
@@ -116,6 +116,8 @@ class Copilot:
                 if "choices" not in line:
                     print("Error:", line)
                     raise Exception(f"No choices on {line}")
+                if len(line["choices"]) == 0:
+                    continue
                 content = line["choices"][0]["delta"]["content"]
                 if content is None:
                     continue
