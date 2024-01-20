f0beab76c489132e30c466d114660ea3ae2f0165
Huynh Duc Dung <dunghd.it@gmail.com>
2024-01-20 21:41:49 +0800

    refactor: create commands base on prompts

--- lua/CopilotChat/init.lua
+++ lua/CopilotChat/init.lua
@@ -2,26 +2,27 @@ local utils = require('CopilotChat.utils')
 
 local M = {}
 
+local default_prompts = {
+  Explain = 'Explain how it works.',
+  Tests = 'Briefly how selected code works then generate unit tests for the code.',
+}
+
 -- Set up the plugin
 ---@param options (table | nil)
 --       - mode: ('newbuffer' | 'split') default: newbuffer.
+--       - prompts: (table?) default: default_prompts.
 M.setup = function(options)
   vim.g.copilot_chat_view_option = options and options.mode or 'newbuffer'
 
-  -- Add new command to explain the selected text with CopilotChat
-  utils.create_cmd('CopilotChatExplain', function()
-    vim.cmd('CopilotChat Explain how it works')
-  end, { nargs = '*', range = true })
+  -- Merge the provided prompts with the default prompts
+  local prompts = vim.tbl_extend('force', default_prompts, options and options.prompts or {})
 
-  -- Add new command to generate unit tests with CopilotChat for selected text
-  utils.create_cmd('CopilotChatTests', function(opts)
-    local cmd = 'CopilotChat Briefly how selected code works then generate unit tests for the code.'
-    -- Append the provided arguments to the command if any
-    if opts.args then
-      cmd = cmd .. ' ' .. opts.args
-    end
-    vim.cmd(cmd)
-  end, { nargs = '*', range = true })
+  --  Loop through merged table and generate commands based on keys.
+  for key, value in pairs(prompts) do
+    utils.create_cmd('CopilotChat' .. key, function()
+      vim.cmd('CopilotChat ' .. value)
+    end, { nargs = '*', range = true })
+  end
 end
 
 return M
