974f14f0d0978d858cbe0126568f30fd63262cb6
Huynh Duc Dung <dunghd.it@gmail.com>
2024-01-27 21:01:08 +0700

    feat: add health check

---
+++ lua/CopilotChat/health.lua
@@ -0,0 +1,76 @@
+local M = {}
+
+local start = vim.health.start or vim.health.report_start
+local warn = vim.health.warn or vim.health.report_warn
+local ok = vim.health.ok or vim.health.report_ok
+
+--- Run a command on an executable and handle potential errors
+---@param executable string
+---@param command string
+local function run_command_on_executable(executable, command)
+  local is_present = vim.fn.executable(executable)
+  if is_present == 0 then
+    return false
+  else
+    local success, result = pcall(vim.fn.system, { executable, command })
+    if success then
+      return result
+    else
+      return false
+    end
+  end
+end
+
+--- Run a python command and handle potential errors
+---@param command string
+local function run_python_command(command)
+  return run_command_on_executable('python3', command)
+end
+
+-- Add health check for python3 and pynvim
+function M.check()
+  start('CopilotChat.nvim health check')
+  local python_version = run_python_command('--version')
+
+  if python_version == false then
+    warn('Python 3 is required')
+    return
+  end
+
+  local major, minor = string.match(python_version, 'Python (%d+)%.(%d+)')
+  if not (major and minor and tonumber(major) >= 3 and tonumber(minor) >= 7) then
+    warn('Python version 3.7 or higher is required')
+  else
+    ok('Python version ' .. major .. '.' .. minor .. ' is supported')
+  end
+
+  -- Create a temporary Python script to check the pynvim version
+  local temp_file = os.tmpname() .. '.py'
+  local file = io.open(temp_file, 'w')
+  if file == nil then
+    warn('Failed to create temporary Python script')
+    return
+  end
+
+  file:write('import pynvim; print(pynvim.__version__)')
+  file:close()
+
+  -- Run the temporary Python script and capture the output
+  local pynvim_version = run_python_command(temp_file)
+
+  -- Trim the output
+  if pynvim_version ~= false then
+    pynvim_version = string.gsub(pynvim_version, '^%s*(.-)%s*$', '%1')
+  end
+
+  -- Delete the temporary Python script
+  os.remove(temp_file)
+
+  if pynvim_version ~= '0.5.0' then
+    warn('pynvim version ' .. pynvim_version .. ' is not supported')
+  else
+    ok('pynvim version ' .. pynvim_version .. ' is supported')
+  end
+end
+
+return M
