c28965e95083365fcba75f924b3866f433c8e5e1
Tomas Slusny <slusnucky@gmail.com>
2024-03-01 15:18:56 +0100

    Fix  plugin_spec.lua (#105)
    Signed-off-by: Tomas Slusny <ts6234@att.com>
    Co-authored-by: Tomas Slusny <ts6234@att.com>

--- test/plugin_spec.lua
+++ test/plugin_spec.lua
@@ -1,7 +1,9 @@
-local plugin = require('CopilotChat')
+-- Mock packages
+package.loaded['plenary.curl'] = {}
+package.loaded['plenary.log'] = {}
 
 describe('CopilotChat plugin', function()
   it('should be able to load', function()
-    assert.truthy(plugin)
+    assert.truthy(require('CopilotChat'))
   end)
 end)
