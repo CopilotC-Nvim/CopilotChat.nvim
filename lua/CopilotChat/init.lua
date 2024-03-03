fea61b9a073207957b9d395448c866ad15c5555e
Huynh Duc Dung <dunghd.it@gmail.com>
2024-03-04 07:15:03 +0800

    fix: only show token count if available

--- lua/CopilotChat/init.lua
+++ lua/CopilotChat/init.lua
@@ -475,7 +475,7 @@ function M.ask(prompt, config, source)
       state.chat.spinner:start()
     end,
     on_done = function(response, token_count)
-      if token_count then
+      if token_count > 0 then
         append('\n\n' .. token_count .. ' tokens used')
       end
       append('\n\n' .. config.separator .. '\n\n')
