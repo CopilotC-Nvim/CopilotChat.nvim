67f5a76c28aec8780d33184dc72df555a225f458
Tomas Slusny <slusnucky@gmail.com>
2024-03-03 13:49:27 +0100

    Now run tiktoken load async properly
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- lua/CopilotChat/tiktoken.lua
+++ lua/CopilotChat/tiktoken.lua
@@ -12,17 +12,6 @@ local function get_cache_path()
   end
 end
 
----Save data to cache
----@param data string[]
-local function save_cached_file(data)
-  local cache_path = get_cache_path()
-  if not cache_path then
-    return nil
-  end
-  -- Write data to c100k.tiktoken file in cache
-  vim.fn.writefile(data, cache_path)
-end
-
 local function file_exists(name)
   local f = io.open(name, 'r')
   if f ~= nil then
@@ -33,22 +22,20 @@ local function file_exists(name)
   end
 end
 
----Get tiktoken data from cache or download it
----@return boolean
-local function load_tiktoken_data()
-  if file_exists(get_cache_path()) then
-    return true
-  else
-    local response =
-      curl.get('https://openaipublic.blob.core.windows.net/encodings/cl100k_base.tiktoken')
-    if response.status == 200 then
-      save_cached_file(vim.split(response.body, '\n'))
-      -- Return the response body split by newline
-      return true
-    else
-      return false
-    end
+--- Load tiktoken data from cache or download it
+local function load_tiktoken_data(done)
+  local cache_path = get_cache_path()
+  if file_exists(cache_path) then
+    done(cache_path)
+    return
   end
+
+  curl.get('https://openaipublic.blob.core.windows.net/encodings/cl100k_base.tiktoken', {
+    output = cache_path,
+    callback = function()
+      done(cache_path)
+    end,
+  })
 end
 
 local M = {}
@@ -59,11 +46,7 @@ function M.setup()
     return
   end
 
-  -- start coroutine to load tiktoken data
-  local co = coroutine.create(function()
-    if not load_tiktoken_data() then
-      error('Failed to get tiktoken data')
-    end
+  load_tiktoken_data(function(path)
     local special_tokens = {}
     special_tokens['<|endoftext|>'] = 100257
     special_tokens['<|fim_prefix|>'] = 100258
@@ -72,10 +55,9 @@ function M.setup()
     special_tokens['<|endofprompt|>'] = 100276
     local pat_str =
       "(?i:'s|'t|'re|'ve|'m|'ll|'d)|[^\\r\\n\\p{L}\\p{N}]?\\p{L}+|\\p{N}{1,3}| ?[^\\s\\p{L}\\p{N}]+[\\r\\n]*|\\s*[\\r\\n]+|\\s+(?!\\S)|\\s+"
-    core.new(get_cache_path(), special_tokens, pat_str)
+    core.new(path, special_tokens, pat_str)
     tiktoken_core = core
   end)
-  coroutine.resume(co)
 end
 
 function M.encode(prompt)
