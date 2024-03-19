164ff227be151dcbe6ad13dfa78625b1ba175935
tlacuilose <email>
2024-03-18 20:21:19 -0600

    Explain setting buf options and remove relative number default

--- lua/CopilotChat/chat.lua
+++ lua/CopilotChat/chat.lua
@@ -157,7 +157,6 @@ function Chat:open(config)
   vim.wo[self.winnr].cursorline = true
   vim.wo[self.winnr].conceallevel = 2
   vim.wo[self.winnr].foldlevel = 99
-  vim.wo[self.winnr].relativenumber = false
   if config.show_folds then
     vim.wo[self.winnr].foldcolumn = '1'
     vim.wo[self.winnr].foldmethod = 'expr'
