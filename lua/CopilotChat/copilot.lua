285e068975e4e5df59e1c5fe92a88acaffcde748
Tomas Slusny <slusnucky@gmail.com>
2024-11-19 20:42:01 +0100

    fix: fix o1-preview non-streaming response parsing
    After refactor to plenary.async we accidentally lost a way to make stream and callbacks
    work the same. So split parsing logic to parse_line function and use it on final response
    as well when the response is non-streaming
    
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- lua/CopilotChat/copilot.lua
+++ lua/CopilotChat/copilot.lua
@@ -654,29 +654,8 @@ function Copilot:ask(prompt, opts)
     job:shutdown(0)
   end
 
-  local function stream_func(err, line, job)
-    if not line or errored or finished then
-      return
-    end
-
-    if self.current_job ~= job_id then
-      finish_stream(nil, job)
-      return
-    end
-
-    if err or vim.startswith(line, '{"error"') then
-      finish_stream('Failed to get response: ' .. (err and vim.inspect(err) or line), job)
-      return
-    end
-
-    if not vim.startswith(line, 'data: ') then
-      return
-    end
-
-    line = line:gsub('^%s*data:%s*', ''):gsub('%s*$', '')
-
-    if line == '[DONE]' then
-      finish_stream(nil, job)
+  local function parse_line(line)
+    if not line then
       return
     end
 
@@ -688,8 +667,7 @@ function Copilot:ask(prompt, opts)
     })
 
     if not ok then
-      finish_stream('Failed to parse response: ' .. vim.inspect(content) .. '\n' .. line, job)
-      return
+      return content
     end
 
     if content.copilot_references then
@@ -727,6 +705,39 @@ function Copilot:ask(prompt, opts)
     full_response = full_response .. content
   end
 
+  local function stream_func(err, line, job)
+    if not line or errored or finished then
+      return
+    end
+
+    if self.current_job ~= job_id then
+      finish_stream(nil, job)
+      return
+    end
+
+    if err or vim.startswith(line, '{"error"') then
+      finish_stream('Failed to get response: ' .. (err and vim.inspect(err) or line), job)
+      return
+    end
+
+    if not vim.startswith(line, 'data: ') then
+      return
+    end
+
+    line = line:gsub('^%s*data:%s*', ''):gsub('%s*$', '')
+
+    if line == '[DONE]' then
+      finish_stream(nil, job)
+      return
+    end
+
+    err = parse_line(line)
+    if err then
+      finish_stream('Failed to parse response: ' .. vim.inspect(err) .. '\n' .. line, job)
+    end
+  end
+
+  local is_stream = not vim.startswith(model, 'o1')
   local body = vim.json.encode(
     generate_ask_request(
       self.history,
@@ -736,7 +747,7 @@ function Copilot:ask(prompt, opts)
       model,
       temperature,
       max_output_tokens,
-      not vim.startswith(model, 'o1')
+      is_stream
     )
   )
 
@@ -749,14 +760,16 @@ function Copilot:ask(prompt, opts)
     url = 'https://api.githubcopilot.com/agents/' .. agent .. '?chat'
   end
 
-  local response, err = curl_post(
-    url,
-    vim.tbl_extend('force', self.request_args, {
-      headers = self:authenticate(),
-      body = temp_file(body),
-      stream = stream_func,
-    })
-  )
+  local args = vim.tbl_extend('force', self.request_args, {
+    headers = self:authenticate(),
+    body = temp_file(body),
+  })
+
+  if is_stream then
+    args.stream = stream_func
+  end
+
+  local response, err = curl_post(url, args)
 
   if self.current_job ~= job_id then
     return nil, nil, nil
@@ -801,6 +814,10 @@ function Copilot:ask(prompt, opts)
   if errored then
     error(full_response)
     return
+  end
+
+  if not is_stream then
+    parse_line(response.body)
   end
 
   if full_response == '' then
