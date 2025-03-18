ac6daa8d11ffbc157e05e0a526aa91a93b1cb0fa
Teo Ljungberg <teo@teoljungberg.com>
2025-03-18 13:59:37 +0100

    Use topleft for vertical when not &splitright

--- lua/CopilotChat/ui/chat.lua
+++ lua/CopilotChat/ui/chat.lua
@@ -305,6 +305,8 @@ function Chat:open(config)
     end
     if vim.api.nvim_get_option_value('splitright', {}) then
       cmd = 'botright ' .. cmd
+    else
+      cmd = 'topleft ' .. cmd
     end
     vim.cmd(cmd)
     self.winnr = vim.api.nvim_get_current_win()
