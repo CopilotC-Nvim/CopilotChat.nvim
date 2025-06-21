3c59d0321ded839afb51aacb9f66f414486762a7
Gennaro Tedesco <g.tedesco@finlink.de>
2025-06-21 15:32:46 +0200

    feat: introduce diagnostics context and prompt

--- lua/CopilotChat/config/prompts.lua
+++ lua/CopilotChat/config/prompts.lua
@@ -165,4 +165,9 @@ return {
     prompt = 'Write commit message for the change with commitizen convention. Keep the title under 50 characters and wrap message at 72 characters. Format as a gitcommit code block.',
     context = 'git:staged',
   },
+
+  Diagnostics = {
+    prompt = 'Fix all LSP warnings and errors. Show different code patches for each diagnostic.',
+    context = { 'diagnostics', 'buffers' },
+  },
 }
